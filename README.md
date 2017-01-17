39a91caeda76790b3b12c2fc912b36fa33ccaf42
Francesco <francesco.malvezzi@unimore.it>
2017-01-17 14:36:10 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,85 @@
+> This project is work in progress.
+
+# Shibboleth Idp3 X509 as External Auth
+
+## Notes
+Tested with Shibboleth Identity Provider 3.3.0 and Google Chrome 47.x. Currently supported U2F device data stores are DummyDataStore (for
+testing only), and [Yubico's U2F Validation server](https://developers.yubico.com/u2fval/).
+
+
+## Motivations
+X509 auth requires the SSLContext to be available to 
+the Auth engine. Unfortunately, if your setup includes 
+jetty9.3 behind a apache2 reverse proxy, there is 
+no simple way to have it, because jetty9.3 
+deprecates `mod_ajp`.
+
+The apache2 `+ExportCertData` won't work pass
+X509 cert data to jetty on mod_http
+
+    <Location /idp/Authn/X509>
+    # tihs does not pass SSLContext via mod_http
+     SSLVerifyClient require
+     SSLVerifyDepth 5
+     SSLOptions -StdEnvVars +ExportCertData
+    </Location>
+
+Following the Shibboleth documentation, where X509 
+flow is defined as 
+
+    This flow is implemented as a special case of the External
+    # from: https://wiki.shibboleth.net/confluence/display/IDP30/X509AuthnConfiguration
+
+The proposed workaround is:
+1. have apache2 turn the X509 user cert data to header 
+via mod_headers:
+    ```
+    <Location /idp/Authn/External>
+    SSLVerifyClient require
+    SSLVerifyDepth 5
+    SSLOptions -StdEnvVars +ExportCertData
+	RequestHeader set SSL_CLIENT_S_DN "%{SSL_CLIENT_S_DN}s"
+        RequestHeader set SSL_CLIENT_I_DN "%{SSL_CLIENT_I_DN}s"
+        RequestHeader set SSL_CLIENT_M_SERIAL "%{SSL_CLIENT_M_SERIAL}s"
+        RequestHeader set SSL_CLIENT_CERT "%{SSL_CLIENT_CERT}s"
+        RequestHeader set SSL_CLIENT_VERIFY "%{SSL_CLIENT_VERIFY}s"
+    </Location>
+    ```
+2. write a specialized ExternalAuthnConfiguration to 
+deal with this case.
+
+##Enable a External auth method on idp3
+
+1. Edit: conf/authn/general-authn.xml and move bean:
+<bean id="authn/External" parent="shibboleth.AuthenticationFlow"
+            p:nonBrowserSupported="false" />
+after bean with id="authn/Password"
+This is required to enable it as ExtendedFlow on Password
+2. edit conf/idp.properties and enable External flow:
+
+    ```
+    # Regular expression matching login flows to enable, e.g. IPAddress|Password
+    #idp.authn.flows= Password
+    idp.authn.flows= Password|External
+    ```
+3. edit conf/authn/password-authn-config.xml to allow Password flow to call External as ExtendedFlow
+<bean id="shibboleth.authn.Password.ExtendedFlows" class="java.lang.String" c:_0="External" />
+4. copy web.xml to edit-webapps:
+
+    ``
+    cp -v webapp/WEB-INF/web.xml edit-webapp/WEB-INF/web.xml
+    ``  
+  and add, after the X509 authentication stanza:
+
+    ```
+    <!-- Servlet protected by container used for External authentication -->
+    <servlet>
+        <servlet-name>X509ExternalAuthHandler</servlet-name>
+        <servlet-class>it.unimore.shibboleth.idp.authn.impl.X509UnimoreAuthServlet</servlet-class>
+        <load-on-startup>4</load-on-startup>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>X509ExternalAuthHandler</servlet-name>
+        <url-pattern>/Authn/External</url-pattern>
+    </servlet-mapping>
+    ```
---
+++ src/main/groovy/it/unimore/shibboleth/idp/authn/impl/X509UnimoreAuthServlet.groovy
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.unimore.shibboleth.idp.authn.impl
+
+import groovy.util.logging.Slf4j
+import net.shibboleth.idp.authn.AbstractValidationAction
+import net.shibboleth.idp.authn.AuthnEventIds
+import net.shibboleth.idp.authn.context.AuthenticationContext
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty
+import org.opensaml.profile.context.ProfileRequestContext
+
+import javax.annotation.Nonnull
+import javax.security.auth.Subject
+
+import net.shibboleth.idp.authn.AuthnEventIds
+import net.shibboleth.idp.authn.ExternalAuthentication
+import net.shibboleth.idp.authn.ExternalAuthenticationException
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty
+import net.shibboleth.utilities.java.support.resolver.CriteriaSet
+import net.shibboleth.idp.authn.principal.UsernamePrincipal
+
+import javax.security.auth.Subject
+import javax.servlet.ServletConfig
+import javax.servlet.ServletException
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.security.Principal
+
+
+@Slf4j
+class X509UnimoreAuthServlet extends HttpServlet {
+
+
+// Checkstyle: CyclomaticComplexity|ReturnCount OFF
+    /** {@inheritDoc} */
+    @Override
+    protected void service(final HttpServletRequest httpRequest, final HttpServletResponse httpResponse)
+            throws ServletException, IOException {
+
+
+        /** Parameter/cookie for bypassing prompt page. */
+        @Nonnull @NotEmpty final String PASSTHROUGH_PARAM = "x509passthrough";
+
+        for (header in httpRequest.getHeaderNames() ) {
+            log.info("header: {}", $it)
+            value = httpRequest.getHeader($it)
+            log.info("with value: {}", value)
+        }
+
+        try {
+            final String key = ExternalAuthentication.startExternalAuthentication(httpRequest);
+            
+            final String passthrough = httpRequest.getParameter(PASSTHROUGH_PARAM);
+            if (passthrough != null && Boolean.parseBoolean(passthrough)) {
+                log.debug("Setting UI passthrough cookie");
+                final Cookie cookie = new Cookie(PASSTHROUGH_PARAM, "1");
+                cookie.setPath(httpRequest.getContextPath());
+                cookie.setMaxAge(60 * 60 * 24 * 365);
+                cookie.setSecure(true);
+                httpResponse.addCookie(cookie);
+            }
+            
+            final Subject subject = new Subject()
+            Principal principal = new UsernamePrincipal("malvezzi")
+            subject.getPrincipals().add(principal);
+            log.info("created subject: {}", subject)
+            log.info("subject principal: {}", principal)
+            httpRequest.setAttribute(ExternalAuthentication.SUBJECT_KEY, subject);
+
+            final String revokeConsent =
+                    httpRequest.getParameter(ExternalAuthentication.REVOKECONSENT_KEY);
+            if (revokeConsent != null && ("1".equals(revokeConsent) || "true".equals(revokeConsent))) {
+                httpRequest.setAttribute(ExternalAuthentication.REVOKECONSENT_KEY, Boolean.TRUE);
+            }
+
+            ExternalAuthentication.finishExternalAuthentication(key, httpRequest, httpResponse);
+            
+        } catch (final ExternalAuthenticationException e) {
+            throw new ServletException("Error processing external authentication request", e);
+        }
+    }
+// Checkstyle: CyclomaticComplexity|ReturnCount ON
+    
+}
